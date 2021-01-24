d713243368c27fbab0585945275e5f2359b71a42
Garrett Newman <garrett.s.newman@gmail.com>
2021-01-24 13:47:56 -0500

    Use redux-persist to persist redux state on a per-tab basis

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,19 +1,41 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  combineReducers,
+  configureStore,
+  ThunkAction,
+  Action,
+} from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import audioSliceReducer from '../features/audioInput/audioSlice';
 import mediaBarReducer from '../features/mediaBar/mediaBarSlice';
 import musiciansReducer from '../features/musicians/musiciansSlice';
 import roomReducer from '../features/room/roomSlice';
 import recordingReducer from '../features/recording/recordingSlice';
 
+// NOTE(gnewman): We use sessionStorage so that we can tell redux-persist to
+// key off of all tabs uniquely. This means we can open new tabs that get fresh
+// redux states but can still have data persist on reload. Same for electron
+// windows.
+const tabID = sessionStorage.tabID
+  ? sessionStorage.tabID
+  : (sessionStorage.tabID = Math.random());
+const persistConfig = {
+  key: tabID,
+  storage,
+};
 export const store = configureStore({
-  reducer: {
-    audio: audioSliceReducer,
-    mediaBar: mediaBarReducer,
-    musicians: musiciansReducer,
-    room: roomReducer,
-    recording: recordingReducer,
-  },
+  reducer: persistReducer(
+    persistConfig,
+    combineReducers({
+      audio: audioSliceReducer,
+      mediaBar: mediaBarReducer,
+      musicians: musiciansReducer,
+      room: roomReducer,
+      recording: recordingReducer,
+    })
+  ),
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
