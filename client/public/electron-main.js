5bca93e397bb756676a563dcfb3fc7f84ae9570c
Garrett Newman <garrett.s.newman@gmail.com>
2021-03-02 18:37:23 -0500

    Save progress

--- client/public/electron-main.js
+++ client/public/electron-main.js
@@ -12,55 +12,122 @@ const { app, BrowserWindow, ipcMain, shell } = require('electron');
 const log = require('electron-log');
 const { autoUpdater } = require('electron-updater');
 var fs = require('fs');
-const { Readable } = require('stream');
+const { Readable, finished: streamFinished } = require('stream');
+const util = require('util');
+const { once } = require('events');
 
 var portAudio = require('naudiodon');
 
-//console.log(portAudio.getDevices());
-var ao = new portAudio.AudioIO({
-  outOptions: {
-    channelCount: 1,
-    sampleFormat: portAudio.SampleFormat32Bit,
-    sampleRate: 48000,
-    deviceId: -1, // Use -1 or omit the deviceId to select the default device
-    closeOnError: true, // Close the stream if an audio error is detected, if set false then just log the error
-  },
-});
-
-//// Create a stream to pipe into the AudioOutput
-//// Note that this does not strip the WAV header so a click will be heard at the beginning
-var rs = fs.createReadStream('test.wav');
-function idMaker() {
-  let index = 0;
-  return {
-    next: function() {
-      return {
-        value: index++,
-        done: false
-      };
+function runAudio(fileName, sleepTime) {
+  //console.log(portAudio.getDevices());
+  var ao = new portAudio.AudioIO({
+    outOptions: {
+      channelCount: 1,
+      sampleFormat: portAudio.SampleFormat32Bit,
+      sampleRate: 48000,
+      deviceId: -1, // Use -1 or omit the deviceId to select the default device
+      closeOnError: true, // Close the stream if an audio error is detected, if set false then just log the error
     },
-    [Symbol.iterator]: function() { return this; }
-  };
-}
-const readable = Readable.from(idMaker());
-////console.log(rs);
+  });
+  ao.on('finish', () => {
+    console.log("ERROR");
+  });
 
-//// Start piping data and start streaming
-rs.pipe(ao);
-ao.start();
-rs.on('data', chunk => {
-  //console.log('wr');
-  ao.write(chunk);
-});
-const ids = idMaker();
-console.log(ids);
-const myIterable = {};
-myIterable[Symbol.iterator] = function* () {
-    yield 1;
-    yield 2;
-    yield 3;
-};
-console.log(myIterable);
+  //// Create a stream to pipe into the AudioOutput
+  //// Note that this does not strip the WAV header so a click will be heard at the beginning
+  var rs = fs.createReadStream(fileName);
+  //function readableStream(data) {
+    //let index = 0;
+    //return {
+      //next: function() {
+        //console.log('get data');
+        //return {
+          //value: data,
+          //done: false
+        //};
+      //},
+      //[Symbol.iterator]: function() { return this; }
+    //};
+  //}
+  //const readable = Readable.from(idMaker());
+  ////console.log(rs);
+
+  const finished = util.promisify(streamFinished); // (A)
+
+  // USE 2.2.5!!!
+  // https://2ality.com/2019/11/nodejs-streams-async-iteration.html
+  //ao.start();
+  function write(data) {
+    return new Promise((resolve) => {
+      ao.write(data, null, resolve);
+    });
+  }
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  async function pipeFileToSpeakers() {
+    let i = 0;
+    let timeDeltaMs = 0;
+    let prevTimeMs = Date.now();
+    for await (const chunk of rs) {
+      const currentTime = Date.now();
+      const chunkMs = chunk.length / 4 / 48;
+      timeDeltaMs += currentTime - prevTimeMs - chunkMs;
+      prevTimeMs = currentTime;
+      console.log('time delta:', timeDeltaMs);
+      if (i === 20)
+        await sleep(sleepTime);
+
+      console.log('before write', Date.now());
+      const result = await write(chunk);
+      console.log('after write', Date.now());
+      //await sleep(100);
+      console.log('result:', String(result));
+      if (result === false) {
+        // Handle backpressure
+        console.log('waito');
+        await once(ao, 'drain');
+        console.log('drained--time to write more!');
+      }
+      i++;
+    }
+    ao.end(); // (C)
+    // Wait until done. Throws if there are errors.
+    await finished(ao);
+  }
+
+  //// Start piping data and start streaming
+  rs.once('readable', async () => {
+    console.log('STAHT');
+    ao.start()
+    await pipeFileToSpeakers();
+  });
+}
+runAudio('test2.wav', 0);
+runAudio('test2.wav', 4000);
+//rs.once('readable', () => {
+  //console.log('STAHT');
+  //const data = rs.read();
+  //console.log(data);
+  //const readable = Readable.from(readableStream(data));
+  //readable.pipe(ao);
+  //ao.start()
+//});
+//readable.pipe(ao);
+//ao.start();
+//rs.on('data', chunk => {
+  ////console.log('wr');
+  //ao.write(chunk, '', console.log)
+//});
+//const ids = idMaker();
+//console.log(ids);
+//const myIterable = {};
+//myIterable[Symbol.iterator] = function* () {
+    //yield 1;
+    //yield 2;
+    //yield 3;
+//};
+//console.log(myIterable);
 //for (const bob of ids) console.log(bob);
 
 // create a sine wave lookup table
